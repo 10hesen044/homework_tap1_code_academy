0439c0f1ffbf32af546d46136955c99ae17ad1ec
10hesen044 <tu201906250@code.edu.az>
2021-10-25 11:29:16 +0400

    project added

---
+++ homework_tap1/homework_tap1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace homework_tap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrsize, sum = 0, count = 0, qaliq;
+
+            Console.WriteLine("arrayin size daxil edin : ");
+            arrsize = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[arrsize];
+
+            Console.WriteLine("arrayin elementlerini daxil edin : ");
+            for (int i = 0; i < arrsize; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arrsize; i++)
+            {
+                while (arr[i] >= 1)
+                {
+                    qaliq = arr[i] % 10;
+                    sum += qaliq;
+                    arr[i] /= 10;
+
+                }
+
+                if (sum < 10)
+                {
+                    count++;
+                }
+                sum = 0;
+            }
+
+            Console.WriteLine("netice : " + count);
+        }
+    }
+}
